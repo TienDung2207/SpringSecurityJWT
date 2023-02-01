661be92cb1e0248b69c231e08c82e1233d19c639
dungntt <svgxh01@gmail.com>
2023-02-01 16:51:13 +0700

    xin chao

--- src/main/java/com/example/demospringsecurityjwt/controllers/AuthController.java
+++ src/main/java/com/example/demospringsecurityjwt/controllers/AuthController.java
@@ -49,7 +49,7 @@ public class AuthController {
 
     @GetMapping("admin")
     public String admin() {
-        log.info("XIN CHAO");
+        log.info("XIN CHAO!");
         return "Page for admin";
     }
 
