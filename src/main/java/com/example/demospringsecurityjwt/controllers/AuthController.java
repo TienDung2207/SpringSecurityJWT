eafa591b011b82a41bad7108e6bfa117af3c6ab6
dungntt <svgxh01@gmail.com>
2023-02-01 16:42:26 +0700

    log!

--- src/main/java/com/example/demospringsecurityjwt/controllers/AuthController.java
+++ src/main/java/com/example/demospringsecurityjwt/controllers/AuthController.java
@@ -49,7 +49,7 @@ public class AuthController {
 
     @GetMapping("admin")
     public String admin() {
-        log.info("DAY LA page admin!");
+        log.info("log!");
         return "Admin";
     }
 
