3ea8f63cfd3cdbcc152095d9b6460550eb05d960
dungntt <svgxh01@gmail.com>
2023-02-02 14:00:16 +0700

    ghi log

--- src/main/java/com/example/demospringsecurityjwt/jwt/JwtAuthFilter.java
+++ src/main/java/com/example/demospringsecurityjwt/jwt/JwtAuthFilter.java
@@ -47,6 +47,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         } catch (Exception ex) {
+            log.info("DAY LA 1 DONG LOG");
             log.error("Fail on set user authentication: {}", ex.getMessage());
         }
         filterChain.doFilter(request, response);
