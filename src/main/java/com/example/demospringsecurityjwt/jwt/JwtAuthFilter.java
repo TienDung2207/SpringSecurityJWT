33083114ba1455a3e8618f5e0e6630152656eabb
dungntt <svgxh01@gmail.com>
2023-02-02 15:10:03 +0700

    S

--- src/main/java/com/example/demospringsecurityjwt/jwt/JwtAuthFilter.java
+++ src/main/java/com/example/demospringsecurityjwt/jwt/JwtAuthFilter.java
@@ -47,7 +47,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         } catch (Exception ex) {
-            log.info("DAY");
+            log.info("DAY S");
             log.error("Fail on set user authentication: {}", ex.getMessage());
         }
         filterChain.doFilter(request, response);
