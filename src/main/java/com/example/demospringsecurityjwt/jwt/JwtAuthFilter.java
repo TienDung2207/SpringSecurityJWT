cd433989f5e22e9611f2d8ea4ba98ecf4abc4b9e
dungntt <svgxh01@gmail.com>
2023-02-03 14:06:30 +0700

    111

--- src/main/java/com/example/demospringsecurityjwt/jwt/JwtAuthFilter.java
+++ src/main/java/com/example/demospringsecurityjwt/jwt/JwtAuthFilter.java
@@ -47,7 +47,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         } catch (Exception ex) {
-            log.error("error");
+            log.error("ERROR");
             log.error("Fail on set user authentication: {}", ex.getMessage());
         }
         filterChain.doFilter(request, response);
